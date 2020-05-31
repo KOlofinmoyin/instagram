d0c7f21e7358a7aeb552ffad3393223aed8b8c9e
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-05-31 18:19:08 +0100

    created new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/read'
+  get 'posts/update'
+  get 'posts/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+  root 'posts#index'
 end
