28371979fcd5f59c49980e999249a6129b4f3eda
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-06-11 11:52:56 +0100

    populated 'edit' view with post content (and error handling for caption).

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-
+  validates :caption, presence: true, length: { minimum: 5 }
+  
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
