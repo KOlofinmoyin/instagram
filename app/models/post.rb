4236d53441a1a2017c4706ee5a42924b027d2f3c
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-06-12 23:24:34 +0100

    still can't figure out why 'delete' action doesnt work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-  validates :caption, presence: true, length: { minimum: 5 }
   
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { :medium => "640x" }, dependent: :destroy
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
