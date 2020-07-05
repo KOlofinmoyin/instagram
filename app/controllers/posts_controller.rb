53f3ae52d4d17b52b30939b20ff9898bc0fc7979
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-07-05 20:26:32 +0100

    (phew)added //= to the 'application.js' and so fixed the failing 'delete' action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,25 @@ class PostsController < ApplicationController
 before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
+          p '[index]'
     @posts = Post.all
   end
 
   def show
+          p '[show]'
   end
 
   def new
+          p '[new]'
       @post = Post.new
   end
 
   def edit
+          p '[edit]'
   end
 
   def create
+      p '[create]'
       if @post = Post.create(post_params)
         flash[:success] = "Your post has been created!"
         redirect_to posts_path
@@ -26,10 +31,8 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
       end
   end
 
-  def read
-  end
-
   def update
+          p '[update]'
       if @post.update(post_params)
         flash[:success] = "Post updated."
         redirect_to post_path(@post)
@@ -40,6 +43,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   end
 
   def destroy
+          p '[destroy]'
       # if @post.destroy
       #   flash[:success] = "Post Deleted."
       #   redirect_to posts_path
@@ -47,7 +51,8 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
       #   flash.now[:alert] = "Delete failed. Please try again."
       #   render 'show'
       # end
-      @post.destroy
+
+      @post.destroy!
       redirect_to posts_path #, :notice => "Your post has been deleted!"
   end
 
@@ -57,6 +62,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   end
 
   def set_post
+          p '[retrieving specific post id]'
     @post = Post.find(params[:id])
   end
 end
