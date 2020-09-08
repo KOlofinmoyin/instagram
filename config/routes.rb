92f68a889af42b501d4a341d57f3f96f95670163
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-08 18:50:33 +0100

    Added the  gem and fixed the AssetNotPrecompiled in development error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :posts
+
+      root to: "posts#index"
+    end
   get 'posts/index'
   get 'posts/create'
   get 'posts/read'
